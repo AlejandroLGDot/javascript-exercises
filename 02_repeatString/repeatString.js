cc320ec56e27d11376c74114b3a4b4d1ed0726f4
AlejandroLGDot <131721761+AlejandroLGDot@users.noreply.github.com>
2023-09-05 13:09:40 -0600

    Solved exercise 2 of JS

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string = '', num) {
+    // Error scenario
+    if (num < 0)
+    {
+        return "ERROR";
+    }
+    
+    let aux = string;
+    string = ''; // Reset value
+    
+    for (let i = 0; i < num; i++)
+    {
+        string += aux;
+    }
 
+    return string;
 };
 
 // Do not edit below this line
