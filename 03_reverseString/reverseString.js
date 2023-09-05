5ec6c76fd09e23ab36b045e4c1e8613e78e3aa28
AlejandroLGDot <131721761+AlejandroLGDot@users.noreply.github.com>
2023-09-05 17:05:57 -0600

    Solved exercises 3-4 of JS

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reverse_array = word.split('');
+    let aux = '';
+    const length = reverse_array.length - 1;
 
+    for (let i = 0; i < reverse_array.length; i++)
+    {
+        aux = reverse_array[i];
+        reverse_array[i] = reverse_array[length - i];
+        reverse_array[length - i] = aux;
+
+        if (i == Math.floor(reverse_array.length / 2)){
+            break;
+        }
+    }
+
+    word = reverse_array.join('');
+
+    return word;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // Checks for each argument / Number added
+    args.forEach((args) => {
+        const index = array.indexOf(args);
+        if (index > -1)
+        {
+            array.splice(index, 1);
+        }
+    });
 
+    return array;
 };
 
 // Do not edit below this line
