f6bbc12a7ead9b893607ddffbccab1800a54eba1
AlejandroLGDot <131721761+AlejandroLGDot@users.noreply.github.com>
2023-09-06 15:55:26 -0600

    Solved exercises 5-7 of JS

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let totalSum = 0;
+    let aux = 0;
+    
+    if (typeof num1 != "number" || typeof num2 != "number"){
+        return 'ERROR';
+    }
 
+    if (num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+
+    // Works in both ways
+    if (num1 > num2){
+        aux = num1;
+        num1 = num2;
+        num2 = aux;
+    }
+
+    while (num1 <= num2){
+        totalSum += num1;
+        num1++;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
